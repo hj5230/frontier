5ad12b90f770c0266ff54a2bde756efa00d084ab
sgh <315070539@qq.com>
2025-09-11 18:59:41 +0200

    feat(project): update project page

--- src/routes/project/index.tsx
+++ src/routes/project/index.tsx
@@ -1,67 +1,159 @@
-import { h, FunctionComponent, VNode } from 'preact'
+import {
+  h,
+  FunctionComponent,
+  VNode,
+  Fragment,
+} from 'preact'
 
+import { useDefinitions } from '@hooks/useDefinitions'
+
+import { DefinitionModule } from '@typings/definition'
+
+import { Flex } from '@themes/flex'
 import { Heading } from '@themes/heading'
-// import { Badge } from '@themes/badge'
-// import { Flex } from '@themes/flex'
-// import { Text } from '@themes/text'
+import { Text } from '@themes/text'
 
-// import { GlowPanel } from '@components/GlowPanel'
-// import { Media } from '@components/Media'
+import { Blockquote } from '@themes/blockquote'
 
-// import project_definition from '@assets/definition.project'
-// import { Typewriter } from '@components/TypeWriter'
-import { ExclamationTriangleIcon } from '@radix-ui/react-icons'
+import { Skeleton } from '@themes/skeleton'
 
-// TODO: move Container to app.tsx for all routes
+import { GlowPanel } from '@components/GlowPanel'
+import { Typewriter } from '@components/TypeWriter'
+
 const Project: FunctionComponent = (): VNode => {
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <ExclamationTriangleIcon width="48" height="48" />
-      <br />
-      <br />
-      <Heading>页面建设中</Heading>
-    </div>
+  const [definitions, loading, error] = useDefinitions(
+    DefinitionModule.PROJECT,
   )
 
-  // return (
-  //   <Flex direction="column" gap="3">
-  //     {project_definition.project.map(p => {
-  //       const children = (
-  //         <Flex direction="column">
-  //           <Flex justify="between">
-  //             <Heading>{p.name}</Heading>
-  //             <Flex>
-  //               {p.keywords &&
-  //                 p.keywords.map(k => (
-  //                   <Badge color={k.color}>{k.text}</Badge>
-  //                 ))}
-  //             </Flex>
-  //             <Text as="p" color="gray" size="2">
-  //               {p.period}
-  //             </Text>
-  //           </Flex>
-  //           <Text>{p.description}</Text>
-  //           <Media type="video" uri={p.media_uri} />
-  //           <Typewriter text={p.comment} />
-  //         </Flex>
-  //       )
+  const themeColors = [
+    '#8888ff',
+    '#ff8888',
+    '#44cc88',
+    '#ffaa44',
+    '#cc44cc',
+    '#44bbff',
+    '#ffbb44',
+    '#44ffbb',
+    '#bb44ff',
+    '#ff44bb',
+  ]
 
-  //       if (p.themeColor) {
-  //         return (
-  //           <GlowPanel
-  //             inputStyle={{
-  //               border: `1px solid ${p.themeColor}`,
-  //             }}
-  //           >
-  //             {children}
-  //           </GlowPanel>
-  //         )
-  //       } else {
-  //         return <GlowPanel>{children}</GlowPanel>
-  //       }
-  //     })}
-  //   </Flex>
-  // )
+  const layout = (content: VNode) => (
+    <Flex direction="column" gap="3">
+      {content}
+    </Flex>
+  )
+
+  if (loading) {
+    return layout(
+      <Fragment>
+        {[1, 2].map((_, index) => (
+          <Skeleton key={index}>
+            <GlowPanel>
+              <div style={{ height: '200px' }} />
+            </GlowPanel>
+          </Skeleton>
+        ))}
+      </Fragment>,
+    )
+  }
+
+  if (error || !definitions) {
+    return layout(
+      <GlowPanel>
+        <div>Error loading work experience information</div>
+      </GlowPanel>,
+    )
+  }
+
+  const { project } = definitions
+
+  return layout(
+    <Fragment>
+      {project.project.map((p, index) => {
+        const children = (
+          <Flex direction="row" gap="3">
+            {/* 左侧媒体区 */}
+            <div
+              style={{
+                flex: '0 0 320px',
+                maxWidth: '900px',
+                minWidth: '500px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                minHeight: '300px',
+              }}
+            >
+              {p.media_uri &&
+                (p.media_uri.includes('bilibili.com') ? (
+                  <iframe
+                    src={p.media_uri}
+                    style={{
+                      width: '100%',
+                      height: '200px',
+                      borderRadius: '12px',
+                      border: 'none',
+                      minHeight: '300px',
+                    }}
+                    allow="fullscreen"
+                  />
+                ) : (
+                  <img
+                    src={p.media_uri}
+                    alt="project media"
+                    style={{
+                      width: '100%',
+                      maxWidth: '300px',
+                      borderRadius: '12px',
+                      border: '1px solid #333',
+                      minHeight: '300px',
+                    }}
+                  />
+                ))}
+            </div>
+            {/* 右侧内容区 */}
+            <Flex
+              direction="column"
+              gap="2"
+              style={{ flex: 1 }}
+            >
+              <Flex justify="between">
+                <Heading size="6">{p.name}</Heading>
+                <Text as="p" color="gray" size="2">
+                  {p.period}
+                </Text>
+              </Flex>
+              {p.description.map((d, descIndex) => (
+                <Blockquote key={descIndex}>
+                  <Typewriter text={d} />
+                </Blockquote>
+              ))}
+              {p.comment && (
+                <Blockquote>
+                  <Typewriter text={p.comment} />
+                </Blockquote>
+              )}
+            </Flex>
+          </Flex>
+        )
+
+        return (
+          <GlowPanel
+            key={index}
+            inputStyle={{
+              border: `1px solid ${themeColors[index % themeColors.length]}`,
+              boxShadow: 'none', // 关闭 glow 效果
+              background: 'rgba(20,24,32,0.85)',
+              minHeight: '320px',
+            }}
+          >
+            {children}
+          </GlowPanel>
+        )
+      })}
+    </Fragment>,
+  )
 }
 
 export default Project
